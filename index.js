f83b2176abb4eb8378e2259fc665f69ea98528e8
Pedro Gulinelli <booora.pg@gmail.com>
2022-10-03 11:22:11 -0300

    frontend commit

--- index.js
+++ index.js
@@ -2,39 +2,54 @@ const PORT = 8000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 const fs = require('fs');
-
-const url = 'https://www.site.com';//o link do site que você quiser;
-
+require('dotenv').config();
 const app = express();
-// app.get('/', (req, res) => {
-//     res.json("this is my app!");
-// });
+//app.use(express.json());
+app.use(cors());
+const url = process.env.LINK_URL;//o link do site que você quiser;
 
-// app.get('/result', () => {});
-// app.post('/', () => {});
-// app.put('/', () => {});
-// app.delete('/', () => {});
+app.get('/', (req, res) => {
+    res.json("this is my app!");
+});
 
-
-axios(url).then(res => {
-    const html = res.data
-    const $ = cheerio.load(html)
-    const articles = [];
-
-    $('.class', html).each(function(){  //o nome da classe onde estão os links;
-        const title = $(this).text();
-        const link =  $(this).find('a').attr('href');
-        articles.push({
-            title,
-            link
+app.get('/result', (req, res) => {
+    axios(url).then((response) => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = [];
+    
+        $('.fc-slice-wrapper', html).each(function(){  //o nome da classe onde estão os links;
+            const title = $(this).text();
+            const link =  $(this).find('a').attr('href');
+            articles.push({
+                title,
+                link
+            });
         });
-    });
-    console.log(articles);
-    fs.writeFile('links.json', JSON.stringify(articles), (err) => { //cria um arquivo json com os links;
-        if(err){ throw new err }
-        else{ console.log("created!"); }
-    })
-}).catch(error => console.log(error));
+        res.json(articles);  
+    }).catch(error => console.log(error));
+});
+
+// axios(url).then(res => {
+//     const html = res.data
+//     const $ = cheerio.load(html)
+//     const articles = [];
+
+//     $('.class', html).each(function(){  //o nome da classe onde estão os links;
+//         const title = $(this).text();
+//         const link =  $(this).find('a').attr('href');
+//         articles.push({
+//             title,
+//             link
+//         });
+//     });
+//     console.log(articles);
+//     fs.writeFile('links.json', JSON.stringify(articles), (err) => { //cria um arquivo json com os links;
+//         if(err){ throw new err }
+//         else{ console.log("created!"); }
+//     })
+// }).catch(error => console.log(error));
 
 app.listen(PORT, () => console.log(`running at ${PORT}`));
